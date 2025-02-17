932513e8561acc66c6cb1251484a1fe7fa23e30d
Riju Kalarickal <rijukl@gmail.com>
2025-02-16 20:41:29 -0800

    Jenkinsfile-updated

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
-provider "aws" {
-    region = "us-east-2"  
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.87.0"
+    }
+  }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0cb91c7de36eed2cb" # us-west-2
-  instance_type = "t2.micro"
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0cb91c7de36eed2cb"
+  instance_type = "t3.micro"
   tags = {
-      Name = "TF-Instance"
+    Name = "Blah"
   }
 }
